b10fceb728a6606fd87516801aae7ba569e1d0a6
EthanLewis <tuk32865@temple.edu>
2021-01-21 18:57:20 -0500

    update

---
+++ wgrep.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+
+{
+
+    char word [50];
+    printf("word?");
+    scanf("%s", word);
+
+
+
+
+    exit(0);
+
+
+
+
+}

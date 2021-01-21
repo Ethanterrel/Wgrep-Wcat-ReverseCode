41878f8a7b51d7ba507a3e14e6cae26a86b8665d
EthanLewis <tuk32865@temple.edu>
2021-01-20 23:38:34 -0500

    update

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#define maxchar 1000
+
+
 
 int main() {
-    printf("Hello, Worldss!\n");
+    FILE * file;
+
+  char string [maxchar];
+  char* filename = "C:\\Users\\versa\\CLionProjects\\Project 0\\First.txt";
+
+    file=fopen(filename, "r");
+
+    if(file== NULL){
+        printf("bad file");
+       return 0;
+    }
+
+    while(fgets(string,maxchar,file) !=NULL)
+        printf( string);
+    fclose(file);
+
+
+
     return 0;
 }

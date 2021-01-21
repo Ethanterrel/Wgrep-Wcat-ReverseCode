31d9a143ac270a6efbe99ca04d667d6e04e62c66
EthanLewis <tuk32865@temple.edu>
2021-01-20 19:04:28 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Project_0.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_0.dir/main.c.obj"
+  "Project_0.exe"
+  "Project_0.exe.manifest"
+  "Project_0.pdb"
+  "libProject_0.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
